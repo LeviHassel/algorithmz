0e36aacd22d55b66ad1165b8dc1fa8f714a53159
Levi Hassel <levihassel@gmail.com>
2017-01-20 20:43:02 -0600

    Minor changes

--- allPairsSP/allPairsSP.cpp
+++ allPairsSP/allPairsSP.cpp
@@ -1,15 +1,14 @@
+#include <vector>
+#include "allPairsSP.h"
+
+using namespace std;
+
 /*
 Name: allPairsSP
 Input: n×n vector (representing the adjacency matrix of i graph) and a value i (between 1 and n)
 Output: n×n vector representing the matrix computed after the ith stage of Floyd’s algorithm
 Note: infinity (∞) will be represented as −1
 */
-
-#include <vector>
-#include "allPairsSP.h"
-
-using namespace std;
-
 vector<vector<double>> allPairsSP(vector<vector<double>> adjacencyMatrix, int i) {
 	for (int j = 0; j < i; j++) {
 		for (int k = 0; k < adjacencyMatrix.size(); k++) {
